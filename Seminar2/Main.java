bb18c68acf70a93b538fcf13182c5e03bb9c335f
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-03-27 22:31:03 +0300

    add...

--- Seminar2/Main.java
+++ Seminar2/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Human first = new Human("Igor");
-        Human second = new Human("Sasha");
+        Human human1 = new Human("Igor");
+
+
+//        Market market1 = new Market();
+//
+//        market1.acceptToMarket(human1);
+//        market1.takeInQueue(human1);
+//        market1.giveOrders();
+//        market1.takeOrders();
+//        market1.releaseFromQueue();
+//        market1.releaseFromMarket();
+//        market1.update();
     }
 }
+
+
+
+//https://github.com/RomanKostikov/Seminar_2_OOP_Java/blob/main/src/main/java/org/example/hw/Market.java
--- Seminar2/Market.java
+++ Seminar2/Market.java
@@ -2,15 +2,22 @@ import java.util.List;
 
 public class Market implements MarketBehaviour, QueueBehaviour {
 
-    private List<Actor> queue;
+    private final List<Actor> queue;
+
+    public Market(List<Actor> queue) {
+        this.queue = queue;
+    }
+
     @Override
     public void acceptToMarket(Actor actors) {
-
+        System.out.println(String.format("%s приходит в магазин", actors));
+        queue.add(actors);
     }
 
     @Override
     public void releaseFromMarket(List<Actor> actors) {
-
+        queue.remove(actors);
+        System.out.println(actors.getName() + " покинул магазин");
     }
 
     @Override
