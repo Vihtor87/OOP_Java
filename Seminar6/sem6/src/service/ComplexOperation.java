44aa1e466c6ee3a486039be8e6dac69d35dcf37a
Viktor Vinogradov <129902488+Vihtor87@users.noreply.github.com>
2024-04-14 21:48:47 +0300

    Delete Seminar6/sem6 directory
    del

--- Seminar6/sem6/src/service/ComplexOperation.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import domen.ComplexNumber;
-
-interface ComplexOperation {
-    ComplexNumber operate(ComplexNumber num1, ComplexNumber num2);
-}
--- Seminar6/sem6/src/service/Logger.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-class Logger {
-
-    public static void log(String message) {
-        System.out.println(message);
-    }
-}
